1813fe79c3a065e46ed3a63d41cf7369a98b3a0f
ngc92 <ngc92@users.noreply.github.com>
2017-06-14 16:38:20 +0200

    function refactoring

--- gym_quadrotor/envs/copter_env.py
+++ gym_quadrotor/envs/copter_env.py
@@ -10,73 +10,50 @@ from . import geo
 
 from gym.envs.classic_control import rendering
 
+def _draw_ground(viewer, center):
+    """ helper function that draws the ground indicator.
+        The parameter center indicates where the screen center is supposed to be.
+    """
+    viewer.draw_line((-10+center, 0.0), (10+center, 0.0))
+    for i in range(-8, 10, 2):
+        pos = round(center / 2) * 2
+        viewer.draw_line((pos+i, 0.0), (pos+i-1, -1.0))
+
+def _draw_copter(viewer, setup, status):
+    # transformed main axis
+    trafo = status.rotation_matrix
+    start = (status.position[0], status.altitude)
+    def draw_prop(p):
+        rotated = np.dot(trafo, setup.propellers[p].position)
+        end     = (start[0]+rotated[0], start[1]+rotated[2])
+        viewer.draw_line(start, end)
+        copter = rendering.make_circle(.1)
+        copter.set_color(0,0,0)
+        copter.add_attr(rendering.Transform(translation=end))
+        viewer.add_onetime(copter)
+   
+    # draw current orientation
+    rotated = np.dot(trafo, [0,0,0.5])
+    viewer.draw_line(start, (start[0]+rotated[0], start[1]+rotated[2]))
+
+    for i in range(4): draw_prop(i)
+
 class CopterEnvBase(gym.Env):
-    def __init__(self):
+    def __init__(self, strict_actions=True):
         self.viewer = None
+        self.setup = CopterSetup()
         self._seed()
+        self._strict_action_space = strict_actions
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def _draw_ground(self, center):
-        """ helper function that draws the ground indicator.
-            The parameter center indicates where the screen center is supposed to be.
-        """
-        self.viewer.draw_line((-10+center, 0.0), (10+center, 0.0))
-        for i in range(-8, 10, 2):
-            pos = round(center / 2) * 2
-            self.viewer.draw_line((pos+i, 0.0), (pos+i-1, -1.0))
-
-    def _draw_copter(self, setup, status):
-        # transformed main axis
-        trafo = status.rotation_matrix
-        start = (status.position[0], status.altitude)
-        def draw_prop(p):
-            rotated = np.dot(trafo, setup.propellers[p].position)
-            end     = (start[0]+rotated[0], start[1]+rotated[2])
-            self.viewer.draw_line(start, end)
-            copter = rendering.make_circle(.1)
-            copter.set_color(0,0,0)
-            copter.add_attr(rendering.Transform(translation=end))
-            self.viewer.add_onetime(copter)
-       
-        # draw current orientation
-        rotated = np.dot(trafo, [0,0,0.5])
-        self.viewer.draw_line(start, (start[0]+rotated[0], start[1]+rotated[2]))
-
-        for i in range(4): draw_prop(i)
-        
-        
-
-
-class CopterEnv(CopterEnvBase):
-    metadata = {
-        'render.modes': ['human', 'rgb_array'],
-        'video.frames_per_second' : 10
-    }
-
-    reward_range = (-1.0, 1.0)
-
-    def __init__(self):
-        super(CopterEnv, self).__init__()
-
-        high = np.array([np.inf]*10)
-        
-        self.setup = CopterSetup()
-        self.observation_space = spaces.Box(-high, high)
-        self.action_space = spaces.Box(0, 1, (4,))
-
-        self.target         = np.zeros(3)
-        self.threshold      =  2 * math.pi / 180
-        self.fail_threshold = 10 * math.pi / 180
-        self._fail_count    = 0
-        
-
     def _step(self, action):
-        assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
+        if self._strict_action_space:
+            assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
         
-        self._control = np.array(action)
+        self._control = self._control_from_action(action)
         for i in range(10):
             simulate(self.copterstatus, self.setup, self._control, 0.01)
 
@@ -92,6 +69,72 @@ class CopterEnv(CopterEnvBase):
 
         self._last_control = self._control
         return self._get_state(), reward, done, {}
+
+    def _render(self, mode='human', close=False):
+        if close:
+            if self.viewer is not None:
+                self.viewer.close()
+                self.viewer = None
+            return
+
+        if self.viewer is None:
+            self.viewer = rendering.Viewer(500, 500)
+            self.center = self.copterstatus.position[0]
+        
+        self.center = 0.9*self.center + 0.1*self.copterstatus.position[0]
+        self.viewer.set_bounds(-7 + self.center, 7 + self.center,-1, 13)
+
+        
+        # draw ground
+        _draw_ground(self.viewer, self.center)
+        _draw_copter(self.viewer, self.setup, self.copterstatus)
+
+        # finally draw stuff related to the task
+        self._draw_task_specific()
+
+        return self.viewer.render(return_rgb_array = mode=='rgb_array')
+
+
+class CopterEnv(CopterEnvBase):
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second' : 10
+    }
+
+    reward_range = (-1.0, 1.0)
+
+    def __init__(self):
+        super(CopterEnv, self).__init__()
+
+        high = np.array([np.inf]*10)
+        
+        self.observation_space = spaces.Box(-high, high)
+        self.action_space = spaces.Box(0, 1, (4,))
+
+        self.target         = np.zeros(3)
+        self.threshold      =  2 * math.pi / 180
+        self.fail_threshold = 10 * math.pi / 180
+        self._fail_count    = 0
+    
+    def _step(self, action):
+        self._control = self._control_from_action(action)
+        for i in range(10):
+            simulate(self.copterstatus, self.setup, self._control, 0.01)
+
+        reward, done = self._calc_reward()
+
+        # random disturbances
+        if self.np_random.rand() < 0.01:
+            self.copterstatus.angular_velocity += self.np_random.uniform(low=-10, high=10, size=(3,)) * math.pi / 180
+
+        # change target 
+        if self.np_random.rand() < 0.01:
+            self.target += self.np_random.uniform(low=-3, high=3, size=(3,)) * math.pi / 180
+
+        self._last_control = self._control
+
+    def _control_from_action(self, action):
+        return np.array(action)
 
     def _calc_reward(self):
         err = np.max(np.abs(self.copterstatus.attitude - self.target))
@@ -140,25 +183,7 @@ class CopterEnv(CopterEnvBase):
         # currently, we ignore position and velocity!
         return np.concatenate([s.attitude, s.angular_velocity, self.target, [s.position[2]]])
 
-    def _render(self, mode='human', close=False):
-        if close:
-            if self.viewer is not None:
-                self.viewer.close()
-                self.viewer = None
-            return
-
-        if self.viewer is None:
-            self.viewer = rendering.Viewer(500,500)
-            self.center = self.copterstatus.position[0]
-        
-        self.center = 0.9*self.center + 0.1*self.copterstatus.position[0]
-        self.viewer.set_bounds(-7 + self.center, 7 + self.center,-1, 13)
-
-        
-        # draw ground
-        self._draw_ground(self.center)
-        self._draw_copter(self.setup, self.copterstatus)
-        
+    def _draw_task_specific(self):      
         # draw target orientation
         start = (self.copterstatus.position[0], self.copterstatus.altitude)
         rotated = np.dot(geo.make_quaternion(self.target[0], self.target[1], self.target[2]).rotation_matrix,
@@ -169,7 +194,3 @@ class CopterEnv(CopterEnvBase):
         else:
             color = (1.0, 0.0, 0.0)
         self.viewer.draw_line(start, (start[0]+rotated[0], start[1]+rotated[2]), color=color)
-        
-        
-
-        return self.viewer.render(return_rgb_array = mode=='rgb_array')
