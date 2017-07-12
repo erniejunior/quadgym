9b2b68422e8bb8adecec3fb52df77d43dc3f49cb
ngc92 <ngc92@users.noreply.github.com>
2017-07-12 10:05:33 +0200

    motor torque

--- gym_quadrotor/envs/copter.py
+++ gym_quadrotor/envs/copter.py
@@ -40,7 +40,7 @@ class CopterStatus(object):
             self.rotor_speeds)
 
     def __str__(self):
-        return "CopterStatus(ṕos=%s, vel=%s, att=%s, avel=%s, rspeed=%s)"%(self.position, self.velocity, self.attitude, self.angular_velocity, 
+        return "CopterStatus(pos=%s, vel=%s, att=%s, avel=%s, rspeed=%s)"%(self.position, self.velocity, self.attitude, self.angular_velocity, 
             self.rotor_speeds)
 
 class CopterSetup(object):
@@ -53,16 +53,19 @@ class CopterSetup(object):
         self.mu = np.array([1, 1, 1, 1]) * 1e-7
         self.lm = np.array([1, 1, 1, 1]) * 1e-7
 
+        # motor data
+        self.motor_torque = 0.0301 # [NM]
+
         self.l = 0.31   # Arm length
         self.m = 0.723  # mass
         self.J = 7.321e-5   # Rotor inertia
         self.iI = np.linalg.inv([[8.678e-3,0,0],[0,8.678e-3,0],[0,0,3.217e-2]]) # inverse Inertia (assumed to be diagonal)
 
         cfg = {'a': self.a, 'b': self.b, 'lm': self.lm, 'mu': self.mu, 'axis': [0,0,-1]}
-        P1 = Propeller(d=1,  p = self.l*np.array([1,0,0]), **cfg)
-        P2 = Propeller(d=1,  p = -self.l*np.array([1,0,0]), **cfg)
-        P3 = Propeller(d=-1, p = self.l*np.array([0,1,0]), **cfg)
-        P4 = Propeller(d=-1, p = -self.l*np.array([0,1,0]), **cfg)
+        P1 = Propeller(d=1,  p = self.l*np.array([1, 0, 0.0]), **cfg)
+        P2 = Propeller(d=1,  p = self.l*np.array([-1, 0, 0.0]), **cfg)
+        P3 = Propeller(d=-1, p = self.l*np.array([0, 1, 0.0]), **cfg)
+        P4 = Propeller(d=-1, p = self.l*np.array([0, -1, 0.0]), **cfg)
 
         self.propellers = [P1, P2, P3, P4]
 
@@ -89,9 +92,11 @@ def calc_accelerations(setup, status, control):
     ang_acc = np.dot(setup.iI, moment)
     rot_acc = np.array(ma)
 
+    motor_torque = control * setup.motor_torque
+
     for i, w in enumerate(ma):
         # TODO scale control to torque
-        rot_acc[i]  = w + setup.propellers[i].direction * (control[i] * 0.0075)
+        rot_acc[i]  = w + setup.propellers[i].direction * motor_torque[i]
         rot_acc[i] /= setup.J
         # TODO the inverse of these torques should act on the copter
     #print(lin_acc)
